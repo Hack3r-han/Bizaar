cacf7963e42817df1dae6087d73343ef4a152306
Hack3r-han <hannahphil27@gmail.com>
2024-03-29 20:38:41 +0100

    Make adjustments to aid deployment process

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom';
 import { RouterProvider } from 'react-router-dom';
 import router from './router/router.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
